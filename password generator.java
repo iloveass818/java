a9528b9be3703381ca73443240171b036231ae5e
iloveass818 <74187896+iloveass818@users.noreply.github.com>
2022-10-06 17:07:14 +0530

    Update password generator.java

--- password generator.java
+++ password generator.java
@@ -1,3 +1,4 @@
+
 import java.util.Random;
 import java.util.random.RandomGenerator;
 
@@ -6,12 +7,36 @@ public static void main(String[] args){
 
 	Random random = new Random();
 	
-	for(int i=0;i<=8;i++) {
+	for(int i=0;i<=5;i++) {
 		
-		int x = random.nextInt(72);
-		String s="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$^&*_+-";
+		
+		String s="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+		int x = random.nextInt(s.length());
 		System.out.print(s.charAt(x));
 			
 			
 
-	}}}
+	}
+	
+
+for(int i1=0;i1<=2;i1++) {
+		
+		
+		String s1="1234567890";
+		int x1 = random.nextInt(s1.length());
+		System.out.print(s1.charAt(x1));
+		
+ 
+}
+
+for(int i2=1;i2==1;i2++) {
+	
+	
+	String s2="!@#$^&_+-";
+	int x2 = random.nextInt(s2.length());
+	System.out.print(s2.charAt(x2));
+	
+}
+
+
+}}
